7127a45ce497de748dd8b322ffbde8976301284b
ehgud1008 <ehgud1008@naver.com>
2023-09-06 00:10:44 +0900

    nav 컴포넌트 Cover

--- app/components/Nav.js
+++ app/components/Nav.js
@@ -1,9 +1,14 @@
+"use client";
+
 import Image from 'next/image';
 import Link from 'next/link';
-import React from 'react'
+import { useState, useEffect } from 'react'
+import {signOut, signIn} from 'next-auth/react';
 
 const Nav = () => {
     const isLogin = true;
+    const [toggleDropdown, setToggleDropdown] = useState(false);
+    
     return (
         <nav className='flex-between w-full mb-16pt-3'>
             {/* 프로젝트 로고 >> 클릭 시 / 으로 */}
@@ -15,20 +20,60 @@ const Nav = () => {
 
             {/* pc 화면 */}
             <div className='sm:flex hidden'>
-                {/* 로그인 시 >> 글 생성, 로그아웃, 프로필*/}
-                <div className='flex gap-3 md:gap-5'>
-                    <Link>
-                    
-                    </Link>
-                </div>
-                {/* 로그아웃 시 >> 로그인, 프로필*/}
+                {isLogin ? (
+                    //로그인 시 >> 글 생성, 로그아웃, 프로필
+                    <div className='flex gap-3 md:gap-5 mt-4 mr-2 ml-2'>
+                        <Link href="/create_prompt" className='black_btn'>
+                            글 생성
+                        </Link>
+                        <button type="button" 
+                            onClick={signOut}
+                            className='outline_btn'>
+                            로그아웃
+                        </button>
+                        <Link href="/profile">
+                            <Image src="/images/profile.png"
+                                width={35} height={35}
+                                className='rounded-rull'
+                            />
+                        </Link>
+                    </div>
+                    //로그아웃 시 >> 로그인, 프로필
+                    ) : (
+                        <div className='flex gap-3 md:gap-5 mt-4 mr-2 ml-2'>
+                            <button type='button'
+                                onClick={signIn}
+                                className='outline_btn'>
+                                로그인
+                            </button>
+                            <Link href="/profile">
+                                <Image src="/images/profile.png"
+                                    width={35} height={35}
+                                    className='rounded-rull'
+                                />
+                            </Link>
+                        </div>
+                    )
+                }
             </div>
-            <Link href ="profile" className='flex gap-2flex-center' >
-                <Image src="/images/profile.png" width={30} height={30}
-                    className='mt-4 mr-2 ml-2'
-                />
-                
-            </Link>
+            {/* 모바일 화면 */}
+            <div className='sm:hidden flex relative'>
+                {isLogin ? (
+                    //프로필이미지 하나
+                    //로그인 시 >> 글 생성, 로그아웃, 프로필
+                    <div className="flex mt-4 mr-2 ml-2">
+                        <Image src="/images/profile.png" width={30} height={30}
+                        />
+
+                    </div>
+                    ) : (
+                        //프로필이미지 하나
+                        //로그아웃 시 >> 로그인, 프로필
+                        <></>
+                    ) 
+                }
+            </div>
+            
 
 
         </nav>
