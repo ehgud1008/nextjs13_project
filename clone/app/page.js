2a14249c2ac310de3e74294f13f85517a83b8488
ehgud1008 <ehgud1008@naver.com>
2023-09-11 17:32:54 +0900

    feed 생성 및 조회 clone

--- clone/app/page.js
+++ clone/app/page.js
@@ -9,7 +9,8 @@ import Provider from "@components/Provider";
 const Home = () => {
     return (
         <section className="w-full flex-center flex-col">
-            <h1 className="head_text text-center">Project
+            <h1 className="head_text text-center">
+                Project
                 <br className="max-md:hidden" />
                 <span className="orange_gradient">NextJS13 & React 18</span>
             </h1>
